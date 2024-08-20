fdf3b59c07a466e542fa713f5190f5a7dd7ef0df
Giira <euan.a.marsh@gmail.com>
2024-08-20 17:07:51 +0100

    Fix bug (initialize pygame)

--- main.py
+++ main.py
@@ -3,18 +3,22 @@ from constants import *
 
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    game_on = True
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-    while game_on:
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
         pygame.display.flip()
+
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
